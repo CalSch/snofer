012c26e0402f9db1946b53b7a27be89d3ae263c6
CalSch <cal.schwick@gmail.com>
2026-03-02 14:48:15 -0600

    oh yeag php now

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <stdint.h>
 #include <regex.h>
 #include <stdbool.h>
+#include <signal.h>
 
 #define LOGd(expr) printf(#expr " = %d\n", (expr))
 
@@ -108,26 +109,133 @@ void send_link(char* line, char* cur_path) {
     dprintf(con_fd, "1%s\t%s\t%s\t70\r\n", text, path, host[0]=='.'?HOSTNAME:host);
 }
 
-char path_prefix[] = "content";
-void do_request(char* path) {
-    /* char* fname = malloc(strlen(path)+strlen(path_prefix)+1); */
-    char fname[MAX_PATH_SIZE+100];
-    fname[0]=0;
-    strcat(fname,path_prefix);
-    strcat(fname,path);
+char* escapeshellarg(char* str) {
+    char *escStr;
+    int i,
+        count = strlen(str),
+            ptr_size = count+3;
 
-    if (strlen(path)==0)
-        strcat(fname,"/index");
-    if (strlen(path)==1 && path[0]=='/')
-        strcat(fname,"index");
-
-    /* printf("fname='%s'\n",fname); */
-
-    if (strstr(fname,"..") != NULL) {
-        /* fname = "notfound"; */
-        printf("sneaky guy...\n");
-        strcpy(fname,"notfound");
+    escStr = (char *) calloc(ptr_size, sizeof(char));
+    if (escStr == NULL) {
+        return NULL;
     }
+    sprintf(escStr, "'");
+
+    for(i=0; i<count; i++) {
+        if (str[i] == '\'') {
+                    ptr_size += 3;
+            escStr = (char *) realloc(escStr,ptr_size * sizeof(char));
+            if (escStr == NULL) {
+                return NULL;
+            }
+            sprintf(escStr, "%s'\\''", escStr);
+        } else {
+            sprintf(escStr, "%s%c", escStr, str[i]);
+        }
+    }
+
+    sprintf(escStr, "%s%c", escStr, '\'');
+    return escStr;
+}
+
+void run_php(char* path, char* query, char* outpath, char* ip) {
+    char template[] = "/tmp/snoferXXXXXX";
+    int temp_fd = mkstemp(template);
+    printf("      temp_fd=%d\n", temp_fd);
+    if (temp_fd == -1) {
+        perror("mkstemp()");
+        exit(1);
+    }
+    char temp_path[100] = {0};
+    snprintf(temp_path, 99, "/proc/%d/fd/%d", getpid(), temp_fd);
+    printf("      temp_path=%s\n", temp_path);
+    char cmd[100] = {0};
+    char* query_escaped = escapeshellarg(query);
+    snprintf(cmd, 99, "php -f %s -- %s %s > %s", path, query_escaped, ip, temp_path);
+    free(query_escaped);
+    printf("      running cmd '%s'\n", cmd);
+    int status = system(cmd);
+
+    strcpy(outpath,temp_path);
+}
+
+char path_prefix[] = "content";
+char fname_buf[MAX_PATH_SIZE + 100];
+
+// 1. is there a ".." in the path? return "notfound"
+// 2. is path empty? append "/"
+// 3. does path end in "/"? append "index"
+// 4. does "gen/[path].php" exist? run php file and return temp file
+// 5. does "static/[path]" exist? return "static/[path]"
+// 6. return "notfound"
+char* resolve_path(char* path, char* query, char* ip) {
+    fname_buf[0]=0;
+    
+    strcpy(fname_buf, path);
+
+    // step 1
+    if (strstr(fname_buf,"..") != NULL) {
+        printf("    sneaky guy...\n");
+        /* strcpy(fname_buf,"notfound"); */
+        /* return fname_buf; */
+        return "notfound";
+    }
+    
+    // step 2
+    if (strlen(fname_buf)==0) {
+        printf("    empty path, adding /\n");
+        strcat(fname_buf,"/");
+    }
+
+    // step 3
+    if (fname_buf[strlen(fname_buf)-1]=='/') {
+        printf("    path ends with /, adding index\n");
+        strcat(fname_buf,"index");
+    }
+    
+    // step 4
+    char gen_name[MAX_PATH_SIZE+100];
+    strcpy(gen_name,"gen/");
+    strcat(gen_name,fname_buf);
+    strcat(gen_name,".php");
+    printf("    checking '%s'\n",gen_name);
+    if (access(gen_name, F_OK) == 0) {
+        printf("    running php file: '%s'\n",gen_name);
+        run_php(gen_name, query, fname_buf, ip);
+        return fname_buf;
+    }
+
+    // step 5
+    char static_name[MAX_PATH_SIZE+100];
+    strcpy(static_name,"static/");
+    strcat(static_name,fname_buf);
+    printf("    checking '%s'\n",static_name);
+    if (access(static_name, F_OK) == 0) {
+        printf("    sending static file: '%s'\n",static_name);
+        strcpy(fname_buf, static_name);
+        return fname_buf;
+    }
+
+
+    return "notfound";
+}
+
+void do_request(char* selector, char* ip) {
+    /* char* fname = malloc(strlen(path)+strlen(path_prefix)+1); */
+    char* copy = strdup(selector);
+    char* path = strtok_r(copy, "\t", &copy);
+    char* query = copy;
+
+    if (path == NULL)
+        path = "";
+
+    /* printf("  selector is '%s'\n",selector); */
+    /* printf("  copy is '%s'\n",copy); */
+    /* printf("  path is '%s'\n",path); */
+    printf("  query is '%s'\n",query);
+    printf("  resolving '%s'\n",path);
+    char* fname = resolve_path(path, query, ip);
+    printf("  resolved to '%s'\n",fname);
 
     FILE* f = fopen(fname,"r");
 
@@ -175,12 +283,19 @@ void do_request(char* path) {
 }
 
 void onexit() {
+    printf("ok bye\n");
     close(con_fd);
     close(sock_fd);
 }
 
-int main(int argc, char** argv) {
+void sigint_handle(int sig) {
+    /* onexit(); */
+    exit(sig);
+}
 
+
+int main(int argc, char** argv) {
+    signal(SIGINT, sigint_handle);
     atexit(onexit);
     struct sockaddr_in server_sockaddr_in; // Initialize the details of the server socket
     server_sockaddr_in.sin_family = AF_INET; // Define socket family AF_INET = internetwork: UDP, TCP, etc.
@@ -244,7 +359,7 @@ int main(int argc, char** argv) {
         remove_newline(path_buf);
         printf("  request: '%s'\n", path_buf);
 
-        do_request(path_buf);
+        do_request(path_buf, inet_ntoa(client_sockaddr_in.sin_addr));
 
         // https://man7.org/linux/man-pages/man2/close.2.html
         // close() closes a file descriptor, so that it no longer refers to any file and may be reused
